9d567b5640a318cbe06cac019896309f34e0798d
Bernd Louis <bernd.louis@gmail.com>
2013-11-03 20:02:34 +0100

    Echo service to get started: /nlpagent/echo/

--- nlpagent/urls.py
+++ nlpagent/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
-
 from nlpagent import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^echo/$', views.echo, name='echo'),
 )
--- nlpagent/views.py
+++ nlpagent/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader # FIXME remove me
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def echo(request):
+    try:
+        msg = request.POST['message']
+    except KeyError:
+        msg = ''
+    return render(request, 'nlpagent/echo.html', {'message' : msg})
